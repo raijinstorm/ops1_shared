cd480b284f2ff1bebca60648a3c07a9542149c7f
Aliaksei Kudzelka <aleksejkudelka4@gmail.com>
2025-10-20 15:07:48 +0000

    Part of lab1

--- lesha/1day/prog1.c
+++ lesha/1day/prog1.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+#define MAX_LENGTH
 #define ERR(source) {perror(source);fprintf(stderr,"Error occurred in %s file, in %d line",__FILE__,__LINE__);exit(EXIT_FAILURE);}
 
 int main() {
+
     fprintf(stdout,"Enter your name: ");
     char *name=NULL;
     size_t name_size=0;
     getline(&name,&name_size,stdin);
-    if (strlen(name)>20) {
+    if (strlen(name)>MAX_LENGTH) {
         ERR("Too long name");
     }
     fprintf(stdout,"Your name is %s",name);
     free(name);
+
+
     return EXIT_SUCCESS;
 }
