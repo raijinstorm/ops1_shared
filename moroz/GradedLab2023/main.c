8eb5be5743b4335a82252a4f9060555ea6dcf703
spdsslg <mmoroz5656@gmail.com>
2025-10-29 21:38:04 +0000

    Finished 1st stage 1st lab2023

--- moroz/GradedLab2023/main.c
+++ moroz/GradedLab2023/main.c
@@ -7,16 +7,53 @@
 #include<ctype.h>
 
 int main() {
-    char optionsLetter[] = {'A', 'B', 'C', 'D'};
-    char* options[] = {"write", "read", "walk", "exit"};
-    for (int i=0;i<4;i++) {
-        fprintf(stdout, "%c. %s\n",optionsLetter[i], options[i]);
-    }
+    while (1) {
+        char optionsLetter[] = {'A', 'B', 'C', 'D'};
+        char* options[] = {"write", "read", "walk", "exit"};
+        for (int i=0;i<4;i++) {
+            fprintf(stdout, "%c. %s\n",optionsLetter[i], options[i]);
+        }
 
-    char choice = getchar();
-    if (toupper(choice) != optionsLetter[0] && choice != optionsLetter[1] && choice != optionsLetter[2] && choice != optionsLetter[3]) {
-        fprintf(stdout, "Invalid command\n");
-        return 1;
+        char* choice = NULL;
+        size_t cap = 0;
+        int len = getline(&choice, &cap, stdin);
+        if (len == -1) {
+            perror("getline");
+            return 1;
+        }
+        if (len>0 && choice[len-1] == '\n') {
+            choice[len-1] = '\0';
+            len--;
+        }
+        choice[0] = toupper(choice[0]);
+        if (len>1 || choice[0] != optionsLetter[0] && choice[0] != optionsLetter[1] && choice[0] != optionsLetter[2] && choice[0] != optionsLetter[3]) {
+            free(choice);
+            fprintf(stdout, "Invalid command\n");
+            return 1;
+        }
+        if (choice[0] == 'D') {
+            free(choice);
+            fprintf(stdout, "Gracefully exiting\n");
+            break;
+        }
+
+        char* filename = NULL;
+        size_t nameSize = 0;
+        getline(&filename,&nameSize,stdin );
+        if (len == -1) {
+            perror("getline");
+            return 1;
+        }
+        struct stat st1;
+        if (!stat(filename, &st1)) {
+            free(filename);
+            free(choice);
+            fprintf(stdout, "File does not exist\n");
+            return 1;
+        }
+        fprintf(stdout, "%s", filename);
+        free(filename);
+        free(choice);
     }
 
     return 0;
