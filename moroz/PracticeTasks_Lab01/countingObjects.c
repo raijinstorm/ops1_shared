d8a9a0d5bb167509e1e8965fc3d0315b94f53f03
spdsslg <mmoroz5656@gmail.com>
2025-11-02 11:44:13 +0000

    Lab01 l1-2 with getcwd and chdir

--- moroz/PracticeTasks_Lab01/countingObjects.c
+++ moroz/PracticeTasks_Lab01/countingObjects.c
@@ -10,31 +10,17 @@
 
 #define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
 
-int main() {
-    char* filename;
-    size_t cap=0;
-    fprintf(stdout, "Enter the directory name: ");
-    fflush(stdout);
-    ssize_t filename_len = getline(&filename, &cap, stdin);
-    if (filename_len == -1) {
-        ERR("getline");
-    }
-    filename[filename_len - 1] = '\0';
-    struct stat st;
-    if (stat(filename, &st) == -1) {
-        ERR("stat");
-    }
-    if (!S_ISDIR(st.st_mode)) {
-        fprintf(stderr, "%s is not a directory!\n", filename);
-        return 1;
-    }
+void scan_dir(char* filename) {
     DIR* dir = opendir(filename);
+    if (!dir) {
+        ERR("opendir");
+    }
     struct dirent* ent;
     int reg_count = 0, link_count = 0, dir_count = 0, other_count = 0;
     errno = 0;
     while ((ent = readdir(dir)) != NULL) {
         struct stat st1;
-        if (stat(ent->d_name, &st1) == -1) {
+        if (lstat(ent->d_name, &st1) == -1) {
             ERR("stat");
         }
         if (S_ISDIR(st1.st_mode)) {
@@ -61,4 +47,38 @@ int main() {
         ERR("closedir");
     }
     fprintf(stdout,"The total number of objects in a folder is: %d\n files: %d\n directories: %d\n links: %d\n other: %d", reg_count+link_count+dir_count+other_count, reg_count, dir_count, link_count, other_count);
+}
+
+int main() {
+    char* filename = NULL;
+    size_t cap=0;
+    fprintf(stdout, "Enter the directory name: ");
+    fflush(stdout);
+    ssize_t filename_len = getline(&filename, &cap, stdin);
+    if (filename_len == -1) {
+        ERR("getline");
+    }
+    filename[filename_len - 1] = '\0';
+    struct stat st;
+    if (stat(filename, &st) == -1) {
+        ERR("stat");
+    }
+    if (!S_ISDIR(st.st_mode)) {
+        fprintf(stderr, "%s is not a directory!\n", filename);
+        return 1;
+    }
+
+    char buf[256];
+    if (!getcwd(buf, sizeof(buf))) {
+        ERR("getcwd");
+    }
+    if (chdir(filename)) {
+        ERR("chdir");
+    }
+    scan_dir(".");
+    if (chdir(buf)) {
+        ERR("chdir");
+    }
+
+    return 0;
 }
