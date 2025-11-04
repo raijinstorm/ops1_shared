727e6b11d3679bf0cd06b0225ec5a685de7f6716
raijinstorm <raijinstorm.myth@gmail.com>
2025-11-04 11:52:22 +0000

    l1 polihs lab solutions

--- orlov/lab1_prep/task3_my.c
+++ orlov/lab1_prep/task3_my.c
@@ -11,15 +11,57 @@
 #define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
 #define MAX_ENV_NUM 1024
 
-void create_env(const char* path)
+char* join_paths(const char* path1, const char* path2)
 {
-    if (mkdir(path, 0755) == -1)
+    char* res;
+    const int l1 = strlen(path1);
+    if (path1[l1 - 1] == '/')
+    {
+        res = malloc(strlen(path1) + strlen(path2) + 1);
+        if (!res)
+            ERR("malloc");
+        strcpy(res, path1);
+    }
+    else
+    {
+        res = malloc(strlen(path1) + strlen(path2) + 2);  // additional space for "/"
+        if (!res)
+            ERR("malloc");
+        strcpy(res, path1);
+        res[l1] = '/';
+        res[l1 + 1] = 0;
+    }
+    return strcat(res, path2);
+}
+
+
+void create_env(const char* name)
+{
+    if (mkdir(name, 0755) == -1)
         ERR("mkdir");
 
-    FILE *f = fopen("requirements", "w");
+    char* path2 = join_paths(name, "requirements");
+    FILE *f = fopen(path2, "w");
     if (!f)
         ERR("creating requirements");
+    free(path2);
     fclose(f);
+}
+
+void add_package(const char* env_name, const char* package)
+{
+    char* req_path = join_paths(env_name, "requirements");
+    FILE *f = fopen(req_path, "r");
+    if (!f)
+    {
+        fprintf(stderr, "the environment does not exist\n");
+        free(req_path);
+        return;
+    }
+    f = fopen(req_path, "a");
+    fwrite(package, 1, strlen(package), f);
+
+    free(req_path);
 }
 
 
@@ -27,7 +69,8 @@ int main(int argc, char *argv[])
 {
     int op_mode = -1;
     char* dir_paths[MAX_ENV_NUM];
-    int env_cnt = 0;
+    char* packages[MAX_ENV_NUM];
+    int env_cnt = 0, package_cnt = 0;
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-c" ) == 0) {
             if ( i + 1 >= argc)
@@ -39,13 +82,16 @@ int main(int argc, char *argv[])
         } else if (strcmp(argv[i], "-v") == 0 && i + 1 < argc) {
             dir_paths[env_cnt] = argv[++i];
             env_cnt++;
-        } else {
+        } else if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
+            packages[package_cnt] = argv[++i];
+            package_cnt++;
+        }
+        else {
             fprintf(stderr, "Unknown or incomplete option: %s\n", argv[i]);
         }
     }
 
-    if (op_mode == 0)
-        create_env(dir_paths[0]);
+    add_package(dir_paths[0], packages[0]);
 
     return EXIT_SUCCESS;
 }
