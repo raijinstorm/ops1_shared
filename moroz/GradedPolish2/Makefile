90c6c046339aa5cadc65f384d7fc8061d1210477
spdsslg <mmoroz5656@gmail.com>
2025-12-08 17:42:09 +0000

    two graded added

---
+++ moroz/GradedPolish2/Makefile
@@ -0,0 +1,20 @@
+override CFLAGS=-Wall -Wextra -Wshadow -Wno-unused-parameter -Wno-unused-const-variable -g -O0 -fsanitize=address,undefined
+
+ifdef CI
+override CFLAGS=-Wall -Wextra -Wshadow -Werror -Wno-unused-parameter -Wno-unused-const-variable
+endif
+
+NAME=sop-collection
+
+.PHONY: clean all tidy
+
+all: ${NAME}
+
+${NAME}: ${NAME}.c
+	gcc $(CFLAGS) -o ${NAME} ${NAME}.c
+
+clean:
+	rm -f ${NAME}
+
+tidy:
+	find . -name "skarbona_*" -delete
--- moroz/w2/mykhailo_moroz/ops-admin.c
+++ moroz/w2/mykhailo_moroz/ops-admin.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -11,6 +12,10 @@
     (kill(0, SIGKILL), perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
 
 #define FILE_MAX_SIZE 512
+
+volatile sig_atomic_t usr1_status = 0;
+volatile sig_atomic_t usr2_status = 0;
+void wait_usr2(char* name, sigset_t oldmask);
 
 ssize_t bulk_read(int fd, char* buf, size_t count)
 {
@@ -47,4 +52,130 @@ void sethandler(void (*f)(int), int sigNo)
         ERR("sigaction");
 }
 
-int main(int argc, char* argv[]) {}
+void reap_children() {
+    pid_t pid;
+    while ((pid = wait(NULL)) > 0);
+    if (pid<0) {
+        if (errno == ECHILD) {
+            return;;
+        }
+        ERR("waitpid");
+    }
+}
+
+void handler_usr1(int sigNo)
+{
+    usr1_status = 1;
+}
+void handler_usr2(int sigNo) {
+    usr2_status = 1;
+}
+
+void child_choice(char* name) {
+    srand(time(NULL)*getpid());
+    int choice = rand()%3;
+    if (choice == 0) {
+        printf("%s received a document. Sending to the archive\n", name);
+    }
+    else {
+        if (strcmp(name, "Franz_Joseph")==0) {
+            printf("%s received a document. Ignoring.\n", name);
+        }
+        else {
+            printf("%s received a document. Passing on to the superintendent\n", name);
+            pid_t ppid = getppid();
+            kill(ppid, SIGUSR2);
+        }
+    }
+}
+
+void wait_usr2(char* name, sigset_t oldmask) {
+    sigset_t mask = oldmask;
+    sigdelset(&mask, SIGUSR2);
+    while (1) {
+        sigsuspend(&mask);
+        child_choice(name);
+    }
+}
+
+void get_subordinates(char* path, char* name, sigset_t oldmask)
+{
+    printf("My name is %s and my PID is %d\n", name, getpid());
+    char dir[512];
+    strcpy(dir, path);
+
+    strcat(path, "/");
+    strcat(path, name);
+    int fd = open(path, O_RDONLY);
+    if (fd == -1)
+    {
+        ERR("open");
+    }
+    char names[FILE_MAX_SIZE+1];
+    ssize_t len = bulk_read(fd, names, FILE_MAX_SIZE);
+    names[len] = '\0';
+    char* token = strtok(names, "\n");
+    while (token!=NULL) {
+        if (strcmp(token, "-")==0) {
+            printf("%s inspecting %s\n", name, token);
+        }
+        else {
+            printf("%s inspecting %s\n", name, token);
+            pid_t s = fork();
+            if (s<0) {
+                ERR("fork");
+            }
+            if (s==0) {
+                char new_path[512];
+                strcpy(new_path, dir);
+                get_subordinates(new_path, token, oldmask);
+                exit(EXIT_SUCCESS);
+            }
+        }
+        token = strtok(NULL, "\n");
+    }
+    close(fd);
+    printf("%s has inspected all subordinates\n", name);
+    wait_usr2(name, oldmask);
+    reap_children();
+    printf("%s is leaving the office\n", name);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc!=3)
+    {
+        usage(argc, argv);
+    }
+
+    sethandler(handler_usr1, SIGUSR1);
+    sethandler(handler_usr2, SIGUSR2);
+    sethandler(SIG_IGN, SIGINT);
+
+    sigset_t mask, oldmask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGUSR1);
+    sigaddset(&mask, SIGUSR2);
+    sigaddset(&mask, SIGINT);
+    if (sigprocmask(SIG_BLOCK, &mask, &oldmask)<0)
+    {
+        ERR("sigprocmask");
+    }
+
+    printf("Waiting for SIGUSR1\n");
+
+    sigsuspend(&oldmask);
+
+    char path[512];
+    strcpy(path, argv[1]);
+    get_subordinates(path, argv[2], oldmask);
+    reap_children();
+
+    if (sigprocmask(SIG_UNBLOCK, &mask, NULL)<0)
+    {
+        ERR("sigprocmask");
+    }
+    return EXIT_SUCCESS;
+}
+
+
