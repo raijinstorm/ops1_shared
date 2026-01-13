f510231696ad67b97864ec689f1889db9f80f16d
spdsslg <mmoroz5656@gmail.com>
2026-01-13 11:24:26 +0000

    PracticeL3 - 2

--- moroz/PracticeL3/mykhailo_moroz/sop-lmap.c
+++ moroz/PracticeL3/mykhailo_moroz/sop-lmap.c
@@ -5,21 +5,53 @@ void usage(int argc, char** argv)
     fprintf(stderr, "USAGE: %s iterations bucket_count test_count worker_count output_file\n", argv[0]);
     exit(EXIT_FAILURE);
 }
-int main(int argc, char** argv)
-{
+
+typedef struct Worker {
+    pthread_t tid;
+    unsigned int seed;
+    int start;
+    int end;
+    int iterations;
+    int bucket_count;
+    int* res;
+}Worker;
+
+void readArgs(int argc, char** argv, int* iterations, int* bucket_count, int* test_count, int* worker_count) {
     if (argc!=6) {
         usage(argc,argv);
     }
 
-    int iterations = atoi(argv[1]);
-    int bucket_count = atoi(argv[2]);
-    int test_count = atoi(argv[3]);
-    int worker_count = atoi(argv[4]);
-    char* output_file = argv[5];
+    *iterations = atoi(argv[1]);
+    *bucket_count = atoi(argv[2]);
+    *test_count = atoi(argv[3]);
+    *worker_count = atoi(argv[4]);
 
-    if (iterations<=0 || bucket_count<=0 || test_count<=0 || worker_count<=0) {
+    if (*iterations<=0 || *bucket_count<=0 || *test_count<=0 || *worker_count<=0) {
         usage(argc,argv);
     }
+}
+
+void* work(void* args) {
+    Worker* worker = (Worker*)args;
+
+    for (int i = worker->start; i<worker->end; i++) {
+        for (int j=0;j<worker->iterations;j++) {
+            int bucketId = rand_r(&worker->seed)%worker->bucket_count;
+            worker->res[i*worker->bucket_count + bucketId] += 1;
+        }
+    }
+    return NULL;
+}
+
+int main(int argc, char** argv)
+{
+    int iterations;
+    int bucket_count;
+    int test_count;
+    int worker_count;
+
+    readArgs(argc, argv, &iterations, &bucket_count, &test_count, &worker_count);
+    char* output_file = argv[5];
 
     srand(time(NULL));
     int* res = calloc(test_count*bucket_count, sizeof(int));
@@ -27,10 +59,35 @@ int main(int argc, char** argv)
         ERR("calloc");
     }
 
-    for (int i=0;i<test_count;i++) {
-        for (int j=0;j<iterations;j++) {
-            int bucketId = rand() % bucket_count;
-            res[i*bucket_count + bucketId] +=1;
+    Worker* workers = malloc(sizeof(Worker)*worker_count);
+    if (!workers) {
+        ERR("malloc");
+    }
+    int chunk = test_count/worker_count;
+    int r = test_count%worker_count;
+    int currId = 0;
+
+    for (int i=0;i<worker_count;i++) {
+        int cnt = chunk;
+        if (i<r) {
+            cnt+=1;
+        }
+        workers[i].seed = rand();
+        workers[i].start = currId;
+        workers[i].end = currId + cnt;
+        workers[i].iterations = iterations;
+        workers[i].bucket_count = bucket_count;
+        workers[i].res = res;
+
+        currId += cnt;
+        if (pthread_create(&workers[i].tid, NULL, work, &workers[i])) {
+            ERR("pthread_create");
+        }
+    }
+
+    for (int i=0;i<worker_count;i++) {
+        if (pthread_join(workers[i].tid, NULL)) {
+            ERR("pthread_join");
         }
     }
 
@@ -66,5 +123,6 @@ int main(int argc, char** argv)
         ERR("fclose");
     }
     free(res);
+    free(workers);
     return 0;
 }
